0b358fabe2bc18c2835e56b280862996ebee666f
Roman Dmowski <roman.dmowski@outlook.com>
2018-10-20 09:47:43 -0400

    rebuild solution in new folder

--- MriC.h
+++ MriC.h
@@ -28,8 +28,8 @@
 // TAO_IDL - Generated from
 // be\be_codegen.cpp:152
 
-#ifndef _TAO_IDL_MRIC_RTQ8OE_H_
-#define _TAO_IDL_MRIC_RTQ8OE_H_
+#ifndef _TAO_IDL_MRIC_W9N3LT_H_
+#define _TAO_IDL_MRIC_W9N3LT_H_
 
 #include /**/ "ace/pre.h"
 
--- MriTypeSupportS.h
+++ MriTypeSupportS.h
@@ -28,8 +28,8 @@
 // TAO_IDL - Generated from
 // be\be_codegen.cpp:461
 
-#ifndef _TAO_IDL_MRITYPESUPPORTS_KFBYH4_H_
-#define _TAO_IDL_MRITYPESUPPORTS_KFBYH4_H_
+#ifndef _TAO_IDL_MRITYPESUPPORTS_AEYFEI_H_
+#define _TAO_IDL_MRITYPESUPPORTS_AEYFEI_H_
 
 #include /**/ "ace/pre.h"
 
